234d3e8595a358dc725b5d6e0df7c80321dae921
maxinewell <maxwell.mark.lu@live.com>
2017-02-08 17:26:45 -0600

    Generalize

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 # this works on python 2
-import urllib, sys, time
+import urllib, sys, time, csv
 from datetime import datetime
 
-urlRegex = "http://data.stackexchange.com/stackoverflow/csv/780780?beginId=%s"
+tbList = ["Posts", "Users", "Comments", "Badges",
+          "CloseAsOffTopicReasonTypes",
+          "CloseReasonTypes",
+          "FlagTypes",
+          "PendingFlags",
+          "PostFeedback",
+          "PostHistory",
+          "PostHistoryTypes",
+          "PostLinks",
+          "PostsWithDeleted",
+          "PostTags",
+          "PostTypes",
+          "ReviewRejectionReasons",
+          "ReviewTaskResults",
+          "ReviewTaskResultTypes",
+          "ReviewTasks",
+          "ReviewTaskStates",
+          "ReviewTaskTypes",
+          "SuggestedEdits",
+          "SuggestedEditVotes",
+          "Tags",
+          "TagSynonyms",
+          "Votes",
+          "VoteTypes"]
+urlRegex = "http://data.stackexchange.com/stackoverflow/csv/781501?TableName=%s&beginId=%s"
+urlSizeRegex = "http://data.stackexchange.com/stackoverflow/csv/781507?tbname=%s"
 # change directoryRegex plz!
 # Mac
 directoryRegex = '/Users/maxinewell/Downloads/SO/%s.csv'
@@ -22,26 +47,40 @@ def reporthook(count, block_size, total_size):
     # avoiding overflow 100%
     percent = min(int(count * block_size * 100 / total_size), 100)
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                    (percent, progress_size / (1024 * 1024), speed, duration))
+                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
 
 
-def donwload(rg):
-    print(str(datetime.now()) + " Download start.")
-    url = urlRegex % (rg)
+def donwload(tb, rg):
+    print(str(datetime.now()) + '\033[1m' + " Id " + str(rg) + " Download start." + '\033[0m')
+    url = urlRegex % (tb, rg)
     dst = directoryRegex % (rg)
     urllib.urlretrieve(url, dst, reporthook)
-    print("\n" + str(datetime.now()) + " File " + str(rg) + ".csv Success!")
+    print("\n" + str(datetime.now()) + '\033[1m' + " File " + str(rg) + ".csv Success!" + '\033[0m')
 
 
-def process(max):
+def process(tb, max):
     cur = 0
     while cur <= max:
-        donwload(cur)
+        donwload(tb, cur)
         cur += 50000
+
+
+def getSize(tb):
+    response = urllib.urlopen(urlSizeRegex % (tb))
+    cr = csv.reader(response)
+    next(cr)
+    return next(cr)[0]
+
 
 
 if __name__ == '__main__':
     # table size 42014360
     # calculated size 33.2 GB total
-    process(42014360)
+    tb = raw_input("What is the TableName: ")
+    if tb in tbList:
+        size = getSize(tb)
+        print "Start download table "+'\033[1m' + tb + '\033[0m' + " contains " + '\033[1m' + size + '\033[0m' + " results."
+        process(tb, int(size))
+    else:
+        sys.exit("Table Name is invalid.")
