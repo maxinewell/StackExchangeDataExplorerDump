1600d21d0b80340be2a038d2b7827368607abb5b
maxinewell <maxwell.mark.lu@live.com>
2017-02-07 20:28:20 -0600

    works

---
+++ main.py
@@ -0,0 +1,26 @@
+import urllib
+from datetime import datetime
+
+urlRegex = "http://data.stackexchange.com/stackoverflow/csv/780780?beginId=%s"
+# change directoryRegex plz!
+directoryRegex = 'D:\CSV\%s.csv'
+
+
+def donwload(rg):
+    file = urllib.URLopener()
+    print(str(datetime.now()) + " Download start.")
+    file.retrieve(urlRegex % (rg), directoryRegex % (rg))
+    print(str(datetime.now()) + " File " + str(rg) + ".csv Success!")
+
+
+def process(max):
+    cur = 0
+    while cur <= max:
+        donwload(cur)
+        cur += 50000
+
+
+if __name__ == '__main__':
+    # table size 42014360
+    # calculated size 33.2 GB total
+    process(42014360)
