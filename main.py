b4e826a309336f5fd8d4720502e924e9e880bc7f
maxinewell <maxwell.mark.lu@live.com>
2017-02-08 18:22:00 -0600

    Error Handler for progress

--- main.py
+++ main.py
@@ -39,18 +39,21 @@ directoryRegex = ''
 
 # progress
 def reporthook(count, block_size, total_size):
-    global start_time
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / (1024 * duration))
-    # avoiding overflow 100%
-    percent = min(int(count * block_size * 100 / total_size), 100)
-    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                     (percent, progress_size / (1024 * 1024), speed, duration))
-    sys.stdout.flush()
+    try:
+        global start_time
+        if count == 0:
+            start_time = time.time()
+            return
+        duration = time.time() - start_time
+        progress_size = int(count * block_size)
+        speed = int(progress_size / (1024 * duration))
+        # avoiding overflow 100%
+        percent = min(int(count * block_size * 100 / total_size), 100)
+        sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                         (percent, progress_size / (1024 * 1024), speed, duration))
+        sys.stdout.flush()
+    except:
+        pass
 
 
 def donwload(tb, rg):
